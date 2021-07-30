66fc0586cfffde12c75d5435c752f0dc5552558c
Mogbonjubade Ayoade <ma@commodots.com>
2021-07-30 14:48:27 +0100

    Publish
    First Publish

---
+++ lib/common/constants.dart
@@ -0,0 +1,29 @@
+library constants;
+
+// Your wordpress website URL
+const String WORDPRESS_URL = "https://www.tv360nigeria.com/";
+
+// Featured category ID (for Home Screen top section)
+const int FEATURED_ID = 2;
+
+// Tab 2 page category name
+const String PAGE2_CATEGORY_NAME = "Headline News";
+
+// Tab 2 page category ID
+const int PAGE2_CATEGORY_ID = 1;
+
+// Custom categories in search tab
+// Array in format
+// ["Category Name", "Image Link", "Category ID"]
+const List<dynamic> CUSTOM_CATEGORIES = [
+  ["Headline News", "assets/boxed/headlines.png", 1],
+  ["Covid-19 Update", "assets/boxed/covid.png", 3125],
+  ["Deji 360", "assets/boxed/deji360.png", 4],
+  ["Livestream", "assets/boxed/livestream.png", 19],
+  ["News", "assets/boxed/news.png", 20],
+  ["Business", "assets/boxed/business.png", 5],
+  ["Politics", "assets/boxed/politics.png", 6],
+  ["Sports", "assets/boxed/sport.png", 7],
+  ["Health", "assets/boxed/health.png", 22],
+  ["International", "assets/boxed/international.png", 21],
+];
